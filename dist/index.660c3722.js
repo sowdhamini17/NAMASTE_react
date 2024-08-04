bec2cbc4a14b1bc1e0efa5b984319e4739e76341
sowdhamini17 <73640839+sowdhamini17@users.noreply.github.com>
2024-08-04 07:57:58 +0000

    ignition&layering

---
+++ dist/index.660c3722.js
@@ -0,0 +1,22 @@
+// nested Element
+const block = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "i am a h1 tag"),
+        React.createElement("h2", {}, "i am a h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "i am a h1 tag"),
+        React.createElement("h2", {}, "i am a h2 tag")
+    ])
+]);
+// const heading = React.createElement("h1",{id:"head"},"hello react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(block);
+
+//# sourceMappingURL=index.660c3722.js.map
