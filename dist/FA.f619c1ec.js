e71eba77c448eeb77af93588a09db9661d30a109
sowdhamini17 <73640839+sowdhamini17@users.noreply.github.com>
2024-08-05 05:13:13 +0000

    useState issue resolvd

--- dist/FA.f619c1ec.js
+++ dist/FA.f619c1ec.js
@@ -27432,324 +27432,6 @@ var _resCard = require("./Res_card");
 var _resCardDefault = parcelHelpers.interopDefault(_resCard);
 var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
-// let listRes = [
-//     {
-//         "info": {
-//             "id": "67645",
-//             "name": "Night Lion by Madhampatty Pakashala",
-//             "cloudinaryImageId": "qgck9fzi17ljdvwboljb",
-//             "locality": "Gauthama Puri Nagar",
-//             "areaName": "Peelamedu",
-//             "costForTwo": "₹300 for two",
-//             "cuisines": [
-//                 "South Indian",
-//                 "Chinese",
-//                 "Haleem"
-//             ],
-//             "avgRating": 3.4,
-//             "parentId": "377170",
-//             "avgRatingString": "4.4",
-//             "totalRatingsString": "10K+",
-//             "sla": {
-//                 "deliveryTime": 27,
-//                 "lastMileTravel": 4.1,
-//                 "serviceability": "SERVICEABLE",
-//                 "slaString": "25-30 mins",
-//                 "lastMileTravelString": "4.1 km",
-//                 "iconType": "ICON_TYPE_EMPTY"
-//             },
-//             "availability": {
-//                 "nextCloseTime": "2024-07-29 03:00:00",
-//                 "opened": true
-//             },
-//             "badges": {},
-//             "isOpen": true,
-//             "type": "F",
-//             "badgesV2": {
-//                 "entityBadges": {
-//                     "imageBased": {},
-//                     "textBased": {},
-//                     "textExtendedBadges": {}
-//                 }
-//             },
-//             "aggregatedDiscountInfoV3": {
-//                 "header": "30% OFF",
-//                 "subHeader": "UPTO ₹75"
-//             },
-//             "differentiatedUi": {
-//                 "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-//                 "differentiatedUiMediaDetails": {
-//                     "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-//                     "lottie": {},
-//                     "video": {}
-//                 }
-//             },
-//             "reviewsSummary": {},
-//             "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-//             "restaurantOfferPresentationInfo": {},
-//             "externalRatings": {
-//                 "aggregatedRating": {
-//                     "rating": "--"
-//                 }
-//             },
-//             "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
-//         },
-//         "analytics": {},
-//         "cta": {
-//             "link": "https://www.swiggy.com/restaurants/night-lion-by-madhampatty-pakashala-gauthama-puri-nagar-peelamedu-coimbatore-67645",
-//             "type": "WEBLINK"
-//         }
-//     },
-//     {
-//         "info": {
-//             "id": "440139",
-//             "name": "Great Indian Khichdi by EatFit",
-//             "cloudinaryImageId": "6e44fd7f1e5cd9967edfe47c10247671",
-//             "locality": "Thanneerpandal main Road",
-//             "areaName": "Peelamedu",
-//             "costForTwo": "₹200 for two",
-//             "cuisines": [
-//                 "Home Food",
-//                 "Indian",
-//                 "North Indian",
-//                 "Healthy Food",
-//                 "Snacks",
-//                 "Desserts",
-//                 "Rajasthani",
-//                 "South Indian",
-//                 "Maharashtrian",
-//                 "Sweets"
-//             ],
-//             "avgRating": 4.3,
-//             "parentId": "319582",
-//             "avgRatingString": "4.3",
-//             "totalRatingsString": "500+",
-//             "sla": {
-//                 "deliveryTime": 26,
-//                 "lastMileTravel": 4.9,
-//                 "serviceability": "SERVICEABLE",
-//                 "slaString": "25-30 mins",
-//                 "lastMileTravelString": "4.9 km",
-//                 "iconType": "ICON_TYPE_EMPTY"
-//             },
-//             "availability": {
-//                 "nextCloseTime": "2024-07-29 00:00:00",
-//                 "opened": true
-//             },
-//             "badges": {
-//                 "imageBadges": [
-//                     {
-//                         "imageId": "v1695133679/badges/Pure_Veg111.png",
-//                         "description": "pureveg"
-//                     }
-//                 ]
-//             },
-//             "isOpen": true,
-//             "type": "F",
-//             "badgesV2": {
-//                 "entityBadges": {
-//                     "imageBased": {
-//                         "badgeObject": [
-//                             {
-//                                 "attributes": {
-//                                     "description": "pureveg",
-//                                     "imageId": "v1695133679/badges/Pure_Veg111.png"
-//                                 }
-//                             }
-//                         ]
-//                     },
-//                     "textBased": {},
-//                     "textExtendedBadges": {}
-//                 }
-//             },
-//             "aggregatedDiscountInfoV3": {
-//                 "header": "ITEMS",
-//                 "subHeader": "AT ₹149"
-//             },
-//             "differentiatedUi": {
-//                 "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-//                 "differentiatedUiMediaDetails": {
-//                     "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-//                     "lottie": {},
-//                     "video": {}
-//                 }
-//             },
-//             "reviewsSummary": {},
-//             "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-//             "restaurantOfferPresentationInfo": {},
-//             "externalRatings": {
-//                 "aggregatedRating": {
-//                     "rating": "--"
-//                 }
-//             },
-//             "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
-//         },
-//         "analytics": {},
-//         "cta": {
-//             "link": "https://www.swiggy.com/restaurants/great-indian-khichdi-by-eatfit-thanneerpandal-main-road-peelamedu-coimbatore-440139",
-//             "type": "WEBLINK"
-//         }
-//     },
-// ]
-const [listRes] = (0, _react.useState)([
-    {
-        "info": {
-            "id": "67645",
-            "name": "Night Lion by Madhampatty Pakashala",
-            "cloudinaryImageId": "qgck9fzi17ljdvwboljb",
-            "locality": "Gauthama Puri Nagar",
-            "areaName": "Peelamedu",
-            "costForTwo": "\u20B9300 for two",
-            "cuisines": [
-                "South Indian",
-                "Chinese",
-                "Haleem"
-            ],
-            "avgRating": 3.4,
-            "parentId": "377170",
-            "avgRatingString": "4.4",
-            "totalRatingsString": "10K+",
-            "sla": {
-                "deliveryTime": 27,
-                "lastMileTravel": 4.1,
-                "serviceability": "SERVICEABLE",
-                "slaString": "25-30 mins",
-                "lastMileTravelString": "4.1 km",
-                "iconType": "ICON_TYPE_EMPTY"
-            },
-            "availability": {
-                "nextCloseTime": "2024-07-29 03:00:00",
-                "opened": true
-            },
-            "badges": {},
-            "isOpen": true,
-            "type": "F",
-            "badgesV2": {
-                "entityBadges": {
-                    "imageBased": {},
-                    "textBased": {},
-                    "textExtendedBadges": {}
-                }
-            },
-            "aggregatedDiscountInfoV3": {
-                "header": "30% OFF",
-                "subHeader": "UPTO \u20B975"
-            },
-            "differentiatedUi": {
-                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-                "differentiatedUiMediaDetails": {
-                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-                    "lottie": {},
-                    "video": {}
-                }
-            },
-            "reviewsSummary": {},
-            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-            "restaurantOfferPresentationInfo": {},
-            "externalRatings": {
-                "aggregatedRating": {
-                    "rating": "--"
-                }
-            },
-            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
-        },
-        "analytics": {},
-        "cta": {
-            "link": "https://www.swiggy.com/restaurants/night-lion-by-madhampatty-pakashala-gauthama-puri-nagar-peelamedu-coimbatore-67645",
-            "type": "WEBLINK"
-        }
-    },
-    {
-        "info": {
-            "id": "440139",
-            "name": "Great Indian Khichdi by EatFit",
-            "cloudinaryImageId": "6e44fd7f1e5cd9967edfe47c10247671",
-            "locality": "Thanneerpandal main Road",
-            "areaName": "Peelamedu",
-            "costForTwo": "\u20B9200 for two",
-            "cuisines": [
-                "Home Food",
-                "Indian",
-                "North Indian",
-                "Healthy Food",
-                "Snacks",
-                "Desserts",
-                "Rajasthani",
-                "South Indian",
-                "Maharashtrian",
-                "Sweets"
-            ],
-            "avgRating": 4.3,
-            "parentId": "319582",
-            "avgRatingString": "4.3",
-            "totalRatingsString": "500+",
-            "sla": {
-                "deliveryTime": 26,
-                "lastMileTravel": 4.9,
-                "serviceability": "SERVICEABLE",
-                "slaString": "25-30 mins",
-                "lastMileTravelString": "4.9 km",
-                "iconType": "ICON_TYPE_EMPTY"
-            },
-            "availability": {
-                "nextCloseTime": "2024-07-29 00:00:00",
-                "opened": true
-            },
-            "badges": {
-                "imageBadges": [
-                    {
-                        "imageId": "v1695133679/badges/Pure_Veg111.png",
-                        "description": "pureveg"
-                    }
-                ]
-            },
-            "isOpen": true,
-            "type": "F",
-            "badgesV2": {
-                "entityBadges": {
-                    "imageBased": {
-                        "badgeObject": [
-                            {
-                                "attributes": {
-                                    "description": "pureveg",
-                                    "imageId": "v1695133679/badges/Pure_Veg111.png"
-                                }
-                            }
-                        ]
-                    },
-                    "textBased": {},
-                    "textExtendedBadges": {}
-                }
-            },
-            "aggregatedDiscountInfoV3": {
-                "header": "ITEMS",
-                "subHeader": "AT \u20B9149"
-            },
-            "differentiatedUi": {
-                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-                "differentiatedUiMediaDetails": {
-                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-                    "lottie": {},
-                    "video": {}
-                }
-            },
-            "reviewsSummary": {},
-            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-            "restaurantOfferPresentationInfo": {},
-            "externalRatings": {
-                "aggregatedRating": {
-                    "rating": "--"
-                }
-            },
-            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
-        },
-        "analytics": {},
-        "cta": {
-            "link": "https://www.swiggy.com/restaurants/great-indian-khichdi-by-eatfit-thanneerpandal-main-road-peelamedu-coimbatore-440139",
-            "type": "WEBLINK"
-        }
-    }
-]);
 const Body = ()=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         className: "body",
@@ -27761,30 +27443,30 @@ const Body = ()=>{
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("button", {
                         className: "filter",
                         onClick: ()=>{
-                            listRes = listRes.filter((restaurants)=>restaurants.info.avgRating > 4);
+                            resList = (0, _mockdataDefault.default).filter((restaurants)=>restaurants.info.avgRating > 4);
                         },
                         children: "TOP RATED Restaurants"
                     }, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 335,
+                        lineNumber: 14,
                         columnNumber: 13
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
                         children: "What's on your mind?"
                     }, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 340,
+                        lineNumber: 19,
                         columnNumber: 13
                     }, undefined)
                 ]
             }, void 0, true, {
                 fileName: "SWIGGY_CLONE/src/components/Body.js",
-                lineNumber: 334,
+                lineNumber: 13,
                 columnNumber: 9
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("br", {}, void 0, false, {
                 fileName: "SWIGGY_CLONE/src/components/Body.js",
-                lineNumber: 342,
+                lineNumber: 21,
                 columnNumber: 9
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -27792,55 +27474,55 @@ const Body = ()=>{
                     "Pizza",
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("br", {}, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 345,
+                        lineNumber: 24,
                         columnNumber: 9
                     }, undefined),
                     "Cheesilicious pizzas to make every day extraordinary.",
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("br", {}, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 347,
+                        lineNumber: 26,
                         columnNumber: 9
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("br", {}, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 348,
+                        lineNumber: 27,
                         columnNumber: 9
                     }, undefined),
                     "Restaurants to explore",
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("br", {}, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 350,
+                        lineNumber: 29,
                         columnNumber: 9
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("br", {}, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 351,
+                        lineNumber: 30,
                         columnNumber: 9
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
                         className: "Res_container",
-                        children: listRes.map((restaurants)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _resCardDefault.default), {
+                        children: (0, _mockdataDefault.default).map((restaurants)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _resCardDefault.default), {
                                 resData: restaurants
                             }, restaurants.info.id, false, {
                                 fileName: "SWIGGY_CLONE/src/components/Body.js",
-                                lineNumber: 358,
+                                lineNumber: 37,
                                 columnNumber: 40
                             }, undefined))
                     }, void 0, false, {
                         fileName: "SWIGGY_CLONE/src/components/Body.js",
-                        lineNumber: 352,
+                        lineNumber: 31,
                         columnNumber: 9
                     }, undefined)
                 ]
             }, void 0, true, {
                 fileName: "SWIGGY_CLONE/src/components/Body.js",
-                lineNumber: 343,
+                lineNumber: 22,
                 columnNumber: 8
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "SWIGGY_CLONE/src/components/Body.js",
-        lineNumber: 332,
+        lineNumber: 11,
         columnNumber: 5
     }, undefined);
 };
